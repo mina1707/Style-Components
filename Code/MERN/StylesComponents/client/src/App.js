da376015010fa14a99dc893b3d46e6153149ef7f
mina1707 <xiomina1707@gmail.com>
2022-08-01 21:46:19 -0700

    first commit

---
+++ Code/MERN/StylesComponents/client/src/App.js
@@ -0,0 +1,15 @@
+import {Container} from './Components/styles/Container.styled'
+
+function App() {
+
+
+  return (
+    
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+  
+  );
+}
+
+export default App;
